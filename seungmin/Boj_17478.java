f8fb7677d6d69d276c9a65a6c226759f86e4a7e8
tmdlas369 <tmddudals369@naver.com>
2023-10-03 23:19:40 +0900

    [BOJ] 재귀함수가 뭔가요? / 실버5
    https://www.acmicpc.net/problem/17478

---
+++ seungmin/Boj_17478.java
@@ -0,0 +1,56 @@
+import java.io.*;
+
+public class Boj_17478 {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringBuilder sb = new StringBuilder();
+    static final String DEPTH = "____";
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        N = Integer.parseInt(br.readLine());
+
+        sb.append("어느 한 컴퓨터공학과 학생이 유명한 교수님을 찾아가 물었다.").append("\n");
+        func(0);
+        System.out.println(sb);
+    }
+
+    static void func(int k) {
+        if (k == N) {
+            for (int i = 0; i < k; i++) {
+                sb.append(DEPTH);
+            }
+            sb.append("\"재귀함수가 뭔가요?\"").append("\n");
+            for (int i = 0; i < k; i++) {
+                sb.append(DEPTH);
+            }
+            sb.append("\"재귀함수는 자기 자신을 호출하는 함수라네\"").append("\n");
+            for (int i = 0; i < k; i++) {
+                sb.append(DEPTH);
+            }
+            sb.append("라고 답변하였지.").append("\n");
+            return;
+        }
+        for (int i = 0; i < k; i++) {
+            sb.append(DEPTH);
+        }
+        sb.append("\"재귀함수가 뭔가요?\"").append("\n");
+        for (int i = 0; i < k; i++) {
+            sb.append(DEPTH);
+        }
+        sb.append("\"잘 들어보게. 옛날옛날 한 산 꼭대기에 이세상 모든 지식을 통달한 선인이 있었어.").append("\n");
+        for (int i = 0; i < k; i++) {
+            sb.append(DEPTH);
+        }
+        sb.append("마을 사람들은 모두 그 선인에게 수많은 질문을 했고, 모두 지혜롭게 대답해 주었지.").append("\n");
+        for (int i = 0; i < k; i++) {
+            sb.append(DEPTH);
+        }
+        sb.append("그의 답은 대부분 옳았다고 하네. 그런데 어느 날, 그 선인에게 한 선비가 찾아와서 물었어.\"").append("\n");
+        func(k + 1);
+        for (int i = 0; i < k; i++) {
+            sb.append(DEPTH);
+        }
+        sb.append("라고 답변하였지.").append("\n");
+
+    }
+}
